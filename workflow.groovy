07307c713c1c643471e4930cb2a3c2cad1905119
Graeme Lockley <graemel@no9.co.za>
2015-02-15 16:09:03 +0200

    Added JaCoCo to the running of unit tests.

--- workflow.groovy
+++ workflow.groovy
@@ -1,6 +1,6 @@
 def execute() {
     stage 'Build - clean install'
-    sh 'mvn clean install'
+    sh 'mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install'
     step $class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'
 
     stage 'Update sonar stats'
